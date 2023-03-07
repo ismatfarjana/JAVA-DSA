5a3bf3b0bdc85e46a700f15e975b570872ddd6cc
Syeda Ismat Farjana <syeda.ismat.farjana@gmail.com>
2023-03-07 11:12:57 +1100

    output and input on java

---
+++ src/com/input/Input.java
@@ -0,0 +1,10 @@
+package com.input;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        System.out.println("How are you?");
+        Scanner input = new Scanner(System.in);
+    }
+}
